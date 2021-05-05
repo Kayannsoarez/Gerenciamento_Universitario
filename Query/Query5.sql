5f2c31361a40df3d5c7c07b8429ee7e167bec6c5
Kayann Soares <kayan.soares1@gmail.com>
2021-05-05 16:10:01 -0300

    Create

---
+++ Query/Query5.sql
@@ -0,0 +1,11 @@
+SELECT dept."Nome"
+FROM public."Departamento" AS dept
+	WHERE dept."ID" IN (
+    	SELECT subquery."ID_Departamento" FROM (
+      		SELECT prof_dept."ID_Departamento", COUNT(*) AS total
+      		FROM public."Prof_Departamento" AS prof_dept
+      		GROUP BY prof_dept."ID_Departamento"
+      		ORDER BY total DESC
+      		LIMIT 1
+  		) AS subquery
+)
